2d795619c2b19ef32f24a97f2e43f1badb0c6462
Sai Dinesh Garapati <dsai@vmware.com>
2020-01-08 11:09:03 +0530

    Added router and updated menuitem hats with HOC to route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/home-page.component'
 
+const Hats = () => {
+  return(
+    <h1>Hats page</h1>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route exact path='/shop/hats' component={Hats}></Route>
+    </Switch>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component{
     render() {
         return (
             <div className="directory-menu">
-            {this.state.sections.map((section) => (
-                <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size}></MenuItem>
+            {this.state.sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps}></MenuItem>
             ))} 
             </div>
         )
